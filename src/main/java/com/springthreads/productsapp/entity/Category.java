609cac571db1a174a277aa706b1c2fc11d825cb7
Sophie LAURENT <sophielaurent92@gmail.com>
2025-01-21 15:55:40 +0100

    Category class (creation)

---
+++ src/main/java/com/springthreads/productsapp/entity/Category.java
@@ -0,0 +1,37 @@
+package com.springthreads.productsapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="category")
+@DynamicUpdate
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "wording", nullable = false)
+	String wording;
+
+	@Column(name = "description")
+	String description;
+
+	@Column(name = "creation_date", nullable = false)
+	@DateTimeFormat(pattern="yyy-mm-dd")
+	LocalDate creationDate;
+
+	@Column(name = "change_date", nullable = false)
+	@DateTimeFormat(pattern="yyy-mm-dd")
+	LocalDate changeDate;
+}
